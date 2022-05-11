910fe224e1fbf9fa700c6226710ed9c1aa1423e4
Markeljan <Markeljan19@gmail.com>
2022-05-11 12:21:17 -0400

    WIP getABIfunciton done

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const appId = "jvb22Emu3AzXUN1A9W6SOPNPGkPoCW4tnh5WGwhI";
 Moralis.start({ serverUrl, appId });
 
 
-
 // -----------------------
 // Moralis Wallet       //
 // -----------------------
@@ -72,23 +71,36 @@ document.getElementById("btn-donate").onclick = function () {
 // Aave Rinkeby Deposit //
 // --------------------------
 
-//First: gets the current pool contract address (sometimes it changes)
-async function getPoolContractAddress() {
+//First: gets the current pool contract address
+async function getPoolContractAddress(_contractAbi) {
+  //console.log(_contractAbi)
 const options = {
   chain: "rinkeby",
   address: "0xBA6378f1c1D046e9EB0F538560BA7558546edF3C",
   function_name: "getPool",
-  abi: abis.poolAddressProvider,
+  //abi: abis.poolAddressProvider,
+  abi: _contractAbi,
   params: { },
 };
  const lendingPoolAddress = await Moralis.Web3API.native.runContractFunction(options);
- console.log(`Current Rinkeby aave-pool-contract address: ${lendingPoolAddress}`);
-//Second: Instantiate pool contract to easily call functions
+ console.log(lendingPoolAddress);
 // const lendingPoolInstance = new web3.eth.Contract(lendingPoolABI, lendingPoolAddress)
-//WIP - Mark
-////////////////////////////////
+
+
 }
-document.getElementById("btn-getPool").onclick = getPoolContractAddress;
+
+document.getElementById("btn-getPool")
+.onclick= function() {
+  let _contractAddress = "0xBA6378f1c1D046e9EB0F538560BA7558546edF3C";
+  getRinkebyABI(_contractAddress)};
+
+function getRinkebyABI(_contractAddress){
+  let json = $.getJSON(`https://api-rinkeby.etherscan.io/api?module=contract&action=getabi&address=${_contractAddress}&apikey=AG1IR692DPYC6M7VPNMEUCXV27N85NEANM`, function(json){
+      return json["result"]
+    }).done(function(data) {
+      getPoolContractAddress(JSON.parse(data.result))
+    });
+  }
 
 //Second: create lending pool instance
 
@@ -163,3 +175,18 @@ function Tabs() {
 }
 
 var connectTabs = new Tabs();
+
+
+
+
+/////////////////////////////////////
+// Get ABI of any verified contract//
+// /////////////////////////////////////
+// function queryABIrinkeby(_contractAddress) {
+
+    
+// }
+
+// //queryABIrinkeby("0xBA6378f1c1D046e9EB0F538560BA7558546edF3C")
+// // .then((json) => { console.log(json)})
+
