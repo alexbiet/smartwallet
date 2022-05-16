9876eca9b248bc23aa140a1c1d9d1c9d440c0da1
Alex Biet <alexandrubiet@gmail.com>
2022-05-17 01:09:30 +0200

    Cards boilerplate

--- main.js
+++ main.js
@@ -467,3 +467,15 @@ window.addEventListener('load', async () => {
 // }
 
 });
+
+
+
+
+///////////////////////////
+/// Bootstrap Tooltips ///
+/////////////////////////
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
