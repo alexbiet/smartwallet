956a8e0385dceb545c390bcc9aabdd01fede0416
Markeljan <Markeljan19@gmail.com>
2022-05-18 10:35:14 -0400

    tokenMaps

---
+++ tokenMaps.js
@@ -0,0 +1,24 @@
+// const ETH = {
+//     id: "ETH",
+//     contractAddress: "0xD1DECc6502cc690Bc85fAf618Da487d886E54Abe", //WETHGateway
+//     aTokenAddress: "0x608D11E704baFb68CfEB154bF7Fd641120e33aD4",  //WETHaToken
+//   }
+// const WETH = {
+//     id: "WETH",
+//     contractAddress: "0xd74047010D77c5901df5b0f9ca518aED56C85e8D",
+//     decimals: 18, 
+//     aTokenAddress: "0x608D11E704baFb68CfEB154bF7Fd641120e33aD4",
+//   }
+// const WBTC = {
+//     id:"WBTC",
+//     contractAddress: "0x124F70a8a3246F177b0067F435f5691Ee4e467DD",
+//     decimals: 8, 
+//     aTokenAddress: "0xeC1d8303b8fa33afB59012Fc3b49458B57883326",
+//   }
+// const DAI = {
+//     id: "DAI",
+//     contractAddress: "0x4aAded56bd7c69861E8654719195fCA9C670EB45",
+//     decimals: 18, 
+//     aTokenAddress: "0x49866611AA7Dc30130Ac6A0DF29217D16FD87bc0",
+//   }
+  
