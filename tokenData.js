5b076d0aaa3213bc88dd3ad90f2bc01c8e193ec3
0xNuggan <82726722+0xNuggan@users.noreply.github.com>
2022-05-20 02:52:58 +0200

    small updates

--- tokenData.js
+++ tokenData.js
@@ -2,12 +2,14 @@ const db = {
     rinkeby: {
       ETH: {
        id: "WETH",
+       name: "Wrapped ETH",
        contractAddress: "0xd74047010D77c5901df5b0f9ca518aED56C85e8D",
        decimals: 18, 
        aTokenAddress: "0x608D11E704baFb68CfEB154bF7Fd641120e33aD4",
      },
      WBTC: {
        id:"WBTC",
+       name: "Wrapped BTC",
        contractAddress: "0x124F70a8a3246F177b0067F435f5691Ee4e467DD",
        decimals: 8, 
        aTokenAddress: "0xeC1d8303b8fa33afB59012Fc3b49458B57883326",
@@ -15,6 +17,7 @@ const db = {
      },
      DAI: {
      id: "DAI",
+     name: "DAI",
      contractAddress: "0x4aAded56bd7c69861E8654719195fCA9C670EB45",
      decimals: 18, 
      aTokenAddress: "0x49866611AA7Dc30130Ac6A0DF29217D16FD87bc0",
@@ -30,24 +33,28 @@ const db = {
    mainnet: {
      ETH: {
        id: "MATIC",
+       name: "MATIC",
        decimals: 18,
        contractAddress: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
  
      },
      WETH: {
       id: "WETH",
+      name: "Wrapped ETH",
       contractAddress: "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
       decimals: 18, 
       aTokenAddress: "0x608D11E704baFb68CfEB154bF7Fd641120e33aD4",
     },
     WBTC: {
       id:"WBTC",
+      name: "Wrapped BTC",
       contractAddress: "0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6",
       decimals: 8, 
       aTokenAddress: "0xeC1d8303b8fa33afB59012Fc3b49458B57883326",
     },
     DAI: {
     id: "DAI",
+    name: "DAI",
     contractAddress: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063",
     decimals: 18, 
     aTokenAddress: "0x49866611AA7Dc30130Ac6A0DF29217D16FD87bc0",
@@ -63,24 +70,28 @@ const db = {
    testnet: {
      ETH: {
        id: "MATIC",
+       name: "MATIC",
        decimals: 18,
        contractAddress: "0xb685400156cF3CBE8725958DeAA61436727A30c3",
  
      },
      WETH: {
       id: "WETH",
+      name: "Wrapped ETH",
       contractAddress: "0x714550C2C1Ea08688607D86ed8EeF4f5E4F22323",
       decimals: 18, 
       aTokenAddress: "0x685bF4eab23993E94b4CFb9383599c926B66cF57",
     },
     WBTC: {
       id:"WBTC",
+      name: "Wrapped BTC",
       contractAddress: "0x85E44420b6137bbc75a85CAB5c9A3371af976FdE",
       decimals: 8, 
       aTokenAddress: "0xde230bC95a03b695be69C44b9AA6C0e9dAc1B143",
     },
     DAI: {
     id: "DAI",
+    name: "DAI",
     contractAddress: "0x9A753f0F7886C9fbF63cF59D0D4423C5eFaCE95B",
     decimals: 18, 
     aTokenAddress: "0xDD4f3Ee61466C4158D394d57f3D4C397E91fBc51",
